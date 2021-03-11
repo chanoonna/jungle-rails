aadb35b98af2d6db72ef22492396f8ef356dd679
chanoonna <chanoonna@gmail.com>
2021-03-11 00:19:41 +0000

    spec: add_to_cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can add an item to cart", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name: Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "Add to cart" do
+    visit root_path
+
+    find('.product').hover
+    find('.btn.btn-primary').click
+    
+    expect(page).to have_content 'My Cart (1)'
+    save_screenshot
+  end
+
+end
